39678153efd6df69af30a712b9278c9a4e0935c5
MuhamedSaleh17 <muhamedsalehgeeklabs@gmail.com>
2025-03-19 16:37:13 +0200

    remove payment routes and refactor server startup logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -16,19 +16,21 @@ app.use(express.json());
 // Routes
 app.use('/stripe', stripeRouter);
 app.use('/client', clientRoute);
-mongoose.connect(process.env.DB_CONNECTION as string)
-    .then(() => {
-        console.log('Connected to MongoDB successfully.');
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
 
 app.get('/', (req, res) => {
     res.send('Hello World from Express and Mongoose!');
 });
 
+// Start the server before MongoDB connection to prevent Render timeout
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Connect to MongoDB separately
+mongoose.connect(process.env.DB_CONNECTION as string)
+    .then(() => {
+        console.log('Connected to MongoDB successfully.');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
