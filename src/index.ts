060ff5da48a5c991479a9c3279be841d5dfafdd2
MuhamedSaleh17 <muhamedsalehgeeklabs@gmail.com>
2025-03-19 16:48:21 +0200

    update middleware comment for clarity

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import clientRoute from './routes/clientRoute/clientRoute';
 
 const app = express();
 
-// Middleware // d4g4sdg4s5dsdgsdgsdg
+// Middleware // d4g4sdg4s5dsdgsdgsdgdsfsdfd
 app.use(express.json());
 
 // Routes
